28a0ec16aca9f4e1af7a43b7dd1c77a2d28556e5
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-04 16:09:29 +0545

    wip: user login schema created

---
+++ src/schema/auth.schema.ts
@@ -0,0 +1,11 @@
+import { Field, ObjectType } from 'type-graphql';
+import { UserSchema } from './user.schema';
+
+@ObjectType()
+export class UserLoginSchema {
+    @Field({ nullable: true })
+    accessToken!: string;
+
+    @Field(() => UserSchema)
+    user!: UserSchema;
+}
