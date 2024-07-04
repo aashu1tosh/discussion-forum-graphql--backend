9b7104bf2b879502bad9facfb5a759ca842c5d54
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-04 12:21:32 +0545

    add: auth entity created

---
+++ src/entities/auth/auth.entity.ts
@@ -0,0 +1,29 @@
+import { Column, Entity } from 'typeorm';
+import { ROLE } from '../../constant/enum';
+import Base from '../base.entity';
+
+@Entity('Auth')
+export class Auth extends Base {
+    @Column()
+    name!: string;
+
+    @Column({
+        unique: true,
+    })
+    email!: string;
+
+    @Column()
+    password!: string;
+
+    @Column({
+        unique: true,
+        nullable: true,
+    })
+    phone!: string;
+
+    @Column({
+        type: 'enum',
+        enum: ROLE,
+    })
+    role!: ROLE;
+}
