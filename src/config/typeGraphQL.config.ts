a2a7b65d5af06a3077218b703a4b043c0904ee2b
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-04 10:48:17 +0545

    feat: schema build using typegraphql

--- src/config/typeGraphQL.config.ts
+++ src/config/typeGraphQL.config.ts
@@ -1,32 +1,17 @@
-/**
- * File: TypeGraphQL.ts
- * Description: This file contains a class for building a GraphQL schema using TypeGraphQL.
- */
-
 import { type GraphQLSchema } from 'graphql'
 import { buildSchema } from 'type-graphql'
 
-/**
- * @class TypeGraphQL
- * @description A class for building a GraphQL schema using TypeGraphQL decorators and resolvers.
- */
-
 export class TypeGraphQL {
-  /**
-   * @method Schema
-   * @returns {Promise<GraphQLSchema>} - A promise that resolves to the GraphQL schema.
-   * @description Builds and returns a GraphQL schema using TypeGraphQL.
-   */
-  async Schema(): Promise<GraphQLSchema> {
-    return await buildSchema({
-      // Resolvers path: Specifies where to find resolver classes using glob pattern.
-      // In this example, it searches for resolver classes in the specified directory and its subdirectories.
-      // eslint-disable-next-line n/no-path-concat
-      resolvers: [__dirname + '/../resolvers/**/*.resolver.ts'],
-      // Validation: Disable automatic schema validation during development.
-      validate: false,
-      // Emit Schema File: Set to false to disable automatic schema file generation.
-      emitSchemaFile: false,
-    })
-  }
+    async schema(): Promise<GraphQLSchema> {
+        return await buildSchema({
+            // Resolvers path: Specifies where to find resolver classes using glob pattern.
+            // In this example, it searches for resolver classes in the specified directory and its subdirectories.
+            resolvers: [__dirname + '/../resolvers/**/*.resolver.ts'],
+            // Validation: Disable automatic schema validation during development.
+            validate: false,
+            // Emit Schema File: Set to false to disable automatic schema file generation.
+            emitSchemaFile: false,
+        })
+    }
 }
+
