785a9d23eeb6af1ac0dc586ec802fe1f1eb0d73f
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-10 12:15:43 +0545

    feat: admin validator for id input

---
+++ src/validator/admin.validator.ts
@@ -0,0 +1,9 @@
+import { IsNotEmpty } from "class-validator";
+import { Field, InputType } from "type-graphql";
+
+@InputType()
+export class IdInput {
+    @Field()
+    @IsNotEmpty()
+    id!: string;
+}
