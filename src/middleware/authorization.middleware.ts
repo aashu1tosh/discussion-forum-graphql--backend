d235d0579a557103753817ce91e725e68285c99b
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-10 12:15:03 +0545

    feat: authorization middleware created

---
+++ src/middleware/authorization.middleware.ts
@@ -0,0 +1,21 @@
+import { NextFunction } from 'express';
+import { MiddlewareFn } from 'type-graphql';
+import { ROLE } from '../constant/enum';
+import { IContext } from '../types/context.type';
+import AppError from '../utils/appError.utils';
+
+export const authorization = (roles: ROLE[]): MiddlewareFn<IContext> => {
+    return async ({ context }: any, next: NextFunction) => {
+        const { res } = context;
+
+        if (!res.locals.id && !res.locals.role)
+            throw AppError.unauthorized("Not authorized for this task");
+        try {
+            const role = res.locals.role;
+            if (roles.includes(role)) return next();
+            else throw AppError.unauthorized("Not authorized for this task");
+        } catch (error) {
+            throw AppError.unauthorized("Not authorized for this task");
+        }
+    };
+};
