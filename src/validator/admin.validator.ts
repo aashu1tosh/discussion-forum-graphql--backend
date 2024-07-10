fa994e1e03d78a20071f1d0bcc4dec71258e8aaf
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-10 12:25:30 +0545

    code format checked

--- src/validator/admin.validator.ts
+++ src/validator/admin.validator.ts
@@ -1,5 +1,5 @@
-import { IsNotEmpty } from "class-validator";
-import { Field, InputType } from "type-graphql";
+import { IsNotEmpty } from 'class-validator';
+import { Field, InputType } from 'type-graphql';
 
 @InputType()
 export class IdInput {
