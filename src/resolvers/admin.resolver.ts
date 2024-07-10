36f818bc669834528929108e34d9eb94e3cd0120
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-10 12:21:40 +0545

    style: format check done

--- src/resolvers/admin.resolver.ts
+++ src/resolvers/admin.resolver.ts
@@ -1,17 +1,15 @@
-import { Arg, Mutation, Query, Resolver, UseMiddleware } from "type-graphql";
-import { ROLE } from "../constant/enum";
-import { RequestValidator } from "../middleware/RequestValidator";
-import { authentication } from "../middleware/authentication.middleware";
-import { authorization } from "../middleware/authorization.middleware";
-import { UserSchema } from "../schema/user.schema";
-import { AdminService } from "../services/admin.service";
-import { IdInput } from "../validator/admin.validator";
+import { Arg, Mutation, Query, Resolver, UseMiddleware } from 'type-graphql';
+import { ROLE } from '../constant/enum';
+import { RequestValidator } from '../middleware/RequestValidator';
+import { authentication } from '../middleware/authentication.middleware';
+import { authorization } from '../middleware/authorization.middleware';
+import { UserSchema } from '../schema/user.schema';
+import { AdminService } from '../services/admin.service';
+import { IdInput } from '../validator/admin.validator';
 
 @Resolver()
 export class AdminResolver {
-    constructor(
-        private readonly adminService = new AdminService(),
-    ) { }
+    constructor(private readonly adminService = new AdminService()) {}
 
     @Query(() => [UserSchema])
     @UseMiddleware(authentication())
@@ -23,10 +21,7 @@ export class AdminResolver {
     @Mutation(() => String)
     @UseMiddleware(RequestValidator.validate(IdInput))
     async deleteUser(@Arg('data') data: IdInput): Promise<string> {
-        const response = await this.adminService.deleteUser(
-            data.id as string
-        );
+        const response = await this.adminService.deleteUser(data.id as string);
         return 'User Deletion Successful';
     }
-
 }
