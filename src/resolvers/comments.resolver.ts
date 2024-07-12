d68cf2e84816a3178e80853b4df5e2f4d64634c2
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-12 11:43:18 +0545

    wip: comment post is now successfull

---
+++ src/resolvers/comments.resolver.ts
@@ -0,0 +1,49 @@
+import {
+    Arg,
+    Ctx,
+    Mutation,
+    Resolver,
+    UseMiddleware
+} from 'type-graphql';
+import { RequestValidator } from '../middleware/RequestValidator';
+import { authentication } from '../middleware/authentication.middleware';
+import { CommentService } from '../services/comment.service';
+import { IContext } from '../types/context.type';
+import {
+    CommentInput,
+    DeleteCommentInput
+} from '../validator/comment.validator';
+import { DeletePostInput } from '../validator/post.validator';
+
+@Resolver()
+export class CommentResolver {
+    constructor(
+        private readonly commentService = new CommentService(),
+    ) { }
+
+    // @Query(() => String)
+    // async getComment() {
+    //     return 'Sending Comments';
+    // }
+
+    @Mutation(() => String)
+    @UseMiddleware(RequestValidator.validate(CommentInput))
+    @UseMiddleware(authentication())
+    async postComment(@Arg('data') data: CommentInput,
+        @Ctx() context: IContext
+    ) {
+        const id = context?.res?.locals?.id
+        await this.commentService.postComment(data, id)
+        return "successful"
+    }
+
+    @Mutation(() => String)
+    @UseMiddleware(RequestValidator.validate(DeleteCommentInput))
+    @UseMiddleware(authentication())
+    async deleteComment(
+        @Arg('data') data: DeletePostInput,
+        @Ctx() context: IContext
+    ): Promise<string> {
+        return 'Comment Deletion Successful';
+    }
+}
