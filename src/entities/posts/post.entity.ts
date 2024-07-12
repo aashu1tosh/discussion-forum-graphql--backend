81ee9144919eed2401b7e534d9081599b83ef712
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-12 14:31:12 +0545

    feat: bidireactional link added to post and comment

--- src/entities/posts/post.entity.ts
+++ src/entities/posts/post.entity.ts
@@ -1,6 +1,7 @@
-import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
 import { Auth } from '../auth/auth.entity';
 import Base from '../base.entity';
+import { Comment } from '../comments/comment.entity';
 
 @Entity('Post')
 export class Post extends Base {
@@ -19,4 +20,7 @@ export class Post extends Base {
 
     @Column()
     user_id!: string; // Foreign key column
+
+    @OneToMany(() => Comment, (comment) => comment.post, { onDelete: 'CASCADE' })
+    comments!: Comment[];
 }
